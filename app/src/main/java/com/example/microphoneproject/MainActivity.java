136052b822d2457b79497050033862180203431d
Noam <noamba1305@gmail.com>
2024-11-17 14:14:00 +0200

    login POC

--- app/src/main/java/com/example/microphoneproject/MainActivity.java
+++ app/src/main/java/com/example/microphoneproject/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.microphoneproject;
 
+import static com.example.microphoneproject.FBRef.refAuth;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     Intent si;
+
+    EditText etEmail;
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        etEmail = findViewById(R.id.editTextUserEmail);
+        etPassword = findViewById(R.id.editTextUserPassword);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = refAuth.getCurrentUser();
+        if(currentUser != null){
+            // put the user details in the fields
+        }
+    }
+
+    public void loginUser(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        refAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = refAuth.getCurrentUser();
+                            //updateUI(user);
+                        }
+                        else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
     }
 
     public void createUser(View view) {
